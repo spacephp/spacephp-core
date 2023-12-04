a4e369f3f760e76e229a3d8e9743ee14f70bb8d0
oangia <blogphp.biz@gmail.com>
2023-12-04 04:56:15 -0500

    U

--- src/Global/Interfaces/IView.php
+++ src/Global/Interfaces/IView.php
@@ -2,7 +2,7 @@
 namespace Illuminate\Global\Interfaces;
 
 interface IView {
-	public static function load($name, $args = []));
+	public static function load($name, $args = []);
 	public static function _404();
 	public static function goBack();
 	public static function partial($name, $args = []);
